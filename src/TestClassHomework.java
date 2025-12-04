10fc41db5e1c579d9f8072b28163a1f4a830138a
tugba <tugbaalkoc0@gmail.com>
2025-12-04 14:43:42 +0300

    Initial commit

---
+++ src/TestClassHomework.java
@@ -0,0 +1,20 @@
+import java.util.Calendar;
+import java.util.Date;
+
+public class TestClass {
+    public static void main(String[] args){
+        Date now = new Date();
+
+        Homework hw1 = new Homework("SENG 211 -OOP","Prepare a slide about array ve arraylist and date class",now);
+        hw1.printInfo();
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(2025,Calendar.DECEMBER,10,23,59);
+        Date deadline =cal.getTime();
+        Homework hw2 = new Homework("CENG 201 - ALGORİTHMS" ,"BST" ,deadline);
+        hw2.printInfo();
+
+
+
+    }
+}
